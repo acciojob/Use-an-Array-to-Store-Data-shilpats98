9c0bddf6f26cacd67571c6280a8f9a2c02310bf8
shilpats98 <118733065+shilpats98@users.noreply.github.com>
2023-02-23 18:47:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var yourArray = [];
-yourArray[0] = 'one';
+yourArray[0] = [ {name : "Rahul" , age : 23}, { job : "Teacher", State : "Karnataka"}]
 yourArray[1] = 'two';
 yourArray[2] = 25;
 yourArray[3] = true;
